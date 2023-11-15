a764ed9ae0f91412387d04386c03fa7b5b1deb8a
PARAS DEO <paras7394@gmail.com>
2023-11-15 10:57:40 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './reducer'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+import "./index.css";
+
+let store = createStore(reducer)
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
